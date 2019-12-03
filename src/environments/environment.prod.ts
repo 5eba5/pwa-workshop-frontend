5cc4cb3686c214ec41fca622bb277fb9cc5b99ab
Sebastian Scheibe <sebastian.scheibe@telent.de>
2019-12-03 09:54:38 +0100

    Step 0: initial setup with non-pwa.

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  config: {
+    // "DATA_API_URL": "https://pwa-workshop-api.herokuapp.com",
+    // "PUSH_API_URL": "https://pwa-workshop-api.herokuapp.com",
+    // "VAPID_PUBLIC_KEY": "BM88mSlUg4mvjcPK5QrzRfQzow91F47iEazCnoTBQ8Hv_AVrJviLcnrNumTK319qWOt43sgOzBJs6UrdOW5IxHg"
+    "DATA_API_URL": "http://localhost:3000",
+    "PUSH_API_URL": "http://localhost:3000",
+    "VAPID_PUBLIC_KEY": "BDdd7gCZBaIveS4cNxpoLwshRncIySUIbIaIMWmBGYCLAS6E0u0Hm4ztVXoC43GGx3Dsvz8CdD57lEgXfOrqX0Q"
+  }
+};
